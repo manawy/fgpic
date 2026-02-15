f92616282a4b61a98f76ace7410321aa03d1dc4f
Fabien Georget <georget@ibac.rwth-aachen.de>
2026-02-15 18:08:40 -0500

    Start work DS3231 RTC driver

---
+++ includes/fgpic/sensors/DS3231.hpp
@@ -0,0 +1,79 @@
+#ifndef FGPIC_SENSORS_DS3231_HPP
+#define FGPIC_SENSORS_DS3231_HPP
+
+#include "fgpic/i2c.hpp"
+#include <cstdint>
+
+struct Time {
+    uint8_t		second;
+    uint8_t		minute;
+    uint8_t		hour;
+    uint8_t		day;
+    uint8_t		date;
+    uint8_t		month;
+    uint16_t	year;
+
+    Time():
+        second(0),
+        minute(0),
+        hour(0),
+        day(0),
+        date(0),
+        month(0),
+        year(0)
+    {};
+};
+
+class DS3231: public I2CDevice
+{
+protected:
+    struct DS3231_REG {
+        static constexpr uint8_t ADDR_I2C = 0x68; // DS3231 address
+
+        static constexpr uint8_t ADDR_START = 0x00;
+
+        static constexpr uint8_t ADDR_SECONDS = 0x00;
+        static constexpr uint8_t ADDR_MINUTES = 0x01;
+        static constexpr uint8_t ADDR_HOURS   = 0x02;
+        static constexpr uint8_t ADDR_DAY     = 0x03;
+        static constexpr uint8_t ADDR_DATE    = 0x04;
+        static constexpr uint8_t ADDR_MONTH   = 0x05;
+        static constexpr uint8_t ADDR_YEAR    = 0x06;
+
+        static constexpr uint8_t ADDR_ALARM1_SECONDS = 0x07;
+        static constexpr uint8_t ADDR_ALARM1_MINUTES = 0x08;
+        static constexpr uint8_t ADDR_ALARM1_HOURS   = 0x09;
+        static constexpr uint8_t ADDR_ALARM1_DAYDATE = 0x0A;
+
+        static constexpr uint8_t ADDR_ALARM2_MINUTES = 0x0B;
+        static constexpr uint8_t ADDR_ALARM2_HOURS   = 0x0C;
+        static constexpr uint8_t ADDR_ALARM2_DAYDATE = 0x0D;
+
+        static constexpr uint8_t ADDR_CONTROL         = 0x0E;
+        static constexpr uint8_t ADDR_CONTROL_STATUS  = 0x0F;
+        static constexpr uint8_t ADDR_AGING_OFFSET    = 0x10;
+        static constexpr uint8_t ADDR_MSB_TEMPERATURE = 0x11;
+        static constexpr uint8_t ADDR_LSB_TEMPERATURE = 0x12;
+    };
+public:
+    DS3231(I2CInterface* i2c_bus);
+
+    void read_time(Time& time);
+
+private:
+
+    uint8_t convert_seconds();
+    uint8_t convert_minutes();
+    uint8_t convert_hours();
+    uint8_t convert_day();
+    uint8_t convert_date();
+    uint8_t convert_month();
+    uint16_t convert_year();
+
+    uint8_t convert_60(uint8_t value);
+
+    uint8_t m_buf[7];
+
+}
+
+#endif // FGPIC_SENSORS_DS3231_HPP
---
+++ src/sensors/DS3231.cpp
@@ -0,0 +1,65 @@
+#include "fgpic/sensors/DS3231.hpp"
+#include "fgpic/i2c.hpp"
+#include <cstdint>
+
+DS3231::DS3231(I2CInterface* i2c_bus) :
+    I2CDevice(i2c_bus, DS3231_REG::ADDR_I2C)
+{}
+
+
+
+
+void DS3231::read_time(Time& time) {
+    m_buf[0] = DS3231_REG::ADDR_START;
+    write_blocking(m_buf, 1, false);
+    read_blocking(m_buf, 7, false);
+
+    time.second = convert_seconds();
+    time.minute = convert_minutes();
+    time.hour = convert_hours();
+    time.day = convert_day();
+    time.date = convert_date();
+    time.month = convert_month();
+    time.year = convert_year();
+}
+
+uint8_t DS3231::convert_seconds() {
+    return convert_60(m_buf[0]);
+}
+
+uint8_t DS3231::convert_minutes() {
+    return convert_60(m_buf[1]);
+}
+
+uint8_t DS3231::convert_hours() {
+    uint8_t decoded;
+    if (m_buf[2] & 128)
+        decoded = (m_buf[2] & 15) + (12 * ((m_buf[2] & 32) >> 5));
+    else
+        decoded = (m_buf[2] & 15) + (10 * ((m_buf[2] & 48) >> 4));
+    return decoded;
+}
+
+uint8_t DS3231::convert_day() {
+    uint8_t decoded = (m_buf[3] & 0x7);
+    return decoded;
+}
+
+uint8_t DS3231::convert_date() {
+    return convert_60(m_buf[4]);
+}
+
+uint8_t DS3231::convert_month() {
+    return convert_60(m_buf[5]);
+}
+
+uint16_t DS3231::convert_year() {
+    uint8_t decoded = (m_buf[6] & 15) + 10 * ((m_buf[6] & (15 << 4)) >> 4);
+    return decoded+2000
+}
+
+uint8_t DS3231::convert_60(uint8_t value) {
+    uint8_t decoded = value & 127;
+    decoded = (decoded & 15) + 10 * ((decoded & (15 << 4)) >> 4);
+    return decoded;
+}
