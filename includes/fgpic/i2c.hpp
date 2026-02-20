a2405a6e430082da948198e762fc2dbc604918cf
Fabien Georget <fabien.georget@usherbrooke.ca>
2026-02-20 15:53:40 -0500

    Add timeout i2c member functions

--- includes/fgpic/i2c.hpp
+++ includes/fgpic/i2c.hpp
@@ -31,19 +31,34 @@ inline I2CInterface::I2CInterface(i2c_inst_t* i2cbus, int sda_pin, int scl_pin):
 class I2CDevice {
 public:
     I2CDevice(I2CInterface* bus, int8_t address);
+    I2CDevice(I2CInterface* bus, int8_t address, uint timeout_us);
+
 
     int write_blocking(const uint8_t *src, size_t len, bool nostop);
+    int write_timeout(const uint8_t *src, size_t len, bool nostop);
 
     int read_blocking(uint8_t *dst, size_t len, bool nostop);
+    int read_timeout(uint8_t *dst, size_t len, bool nostop);
+
+    uint get_timeout() {return m_timeout_us;}
+    void set_timeout(uint timeout_us) {m_timeout_us = timeout_us;}
 private:
     I2CInterface* m_bus;
     const int8_t m_address;
+    uint m_timeout_us;
 
 };
 
 inline I2CDevice::I2CDevice(I2CInterface *bus, int8_t address):
     m_bus(bus),
-    m_address(address)
+    m_address(address),
+    m_timeout_us(10)
+{}
+
+inline I2CDevice::I2CDevice(I2CInterface *bus, int8_t address, uint timeout_us):
+    m_bus(bus),
+    m_address(address),
+    m_timeout_us(timeout_us)
 {}
 
 #endif // FGPIC_I2C_HPP
